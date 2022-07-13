633023e24946ee6096de0acdea8055f9c4c4942b
Yuya Hashizaki <hashizaki@outlook.jp>
2022-07-13 23:54:36 +0900

    dep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'taskCopy-app';
+  url = "https://demo.lychee-redmine.jp/issues.atom?key=b8f5cc90735681ad833885fb280ad19f88dfb524&query_id=1"
+  xml: any
+
+  httpOptions = {
+    headers: new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded'),
+  };
+  constructor(private http: HttpClient,
+) { }
+
+  ngOnInit(): void {
+    this.getTaskXml()
+  }
+
+  getTaskReq(): Observable<any> {
+    return this.http.get(this.url)
+  }
+
+  getTaskXml() {
+    console.log(this.getTaskReq().subscribe(res => { this.xml = res }))
+    
+    // console.log(this.xmlParser.parse(this.xml, this.url))
+  }
+
 }
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -9,28 +9,9 @@ import { Observable } from 'rxjs';
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent implements OnInit {
-
-  url = "https://demo.lychee-redmine.jp/issues.atom?key=b8f5cc90735681ad833885fb280ad19f88dfb524&query_id=1"
-  xml: any
-
-  httpOptions = {
-    headers: new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded'),
-  };
-  constructor(private http: HttpClient,
-) { }
-
   ngOnInit(): void {
-    this.getTaskXml()
+    throw new Error('Method not implemented.');
   }
 
-  getTaskReq(): Observable<any> {
-    return this.http.get(this.url)
-  }
-
-  getTaskXml() {
-    console.log(this.getTaskReq().subscribe(res => { this.xml = res }))
-    
-    // console.log(this.xmlParser.parse(this.xml, this.url))
-  }
-
+ 
 }
